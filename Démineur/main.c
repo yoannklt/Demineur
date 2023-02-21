8a918c44147b7a27375f746f9d61b2c3c3706650
yoannklt <ykerlogot@gaming.tech>
2023-02-21 18:03:27 +0100

    ss

--- Démineur/main.c
+++ Démineur/main.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <time.h>
 
-#define GRID_LENGHT 10
+#define GRID_LENGTH 10
 #define BOMB_NUMBER 17
 
-void displayGrid(unsigned char tableau[GRID_LENGHT][GRID_LENGHT]);
-void placeBomb(unsigned char tableau[GRID_LENGHT][GRID_LENGHT], unsigned int quantity);
-void play(unsigned char tableau[GRID_LENGHT][GRID_LENGHT], unsigned int x, unsigned int y);
+void displayGrid(unsigned char tableau[GRID_LENGTH][GRID_LENGTH]);
+void placeBomb(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int quantity);
+void play(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int x, unsigned int y);
 
 int main(int argc, char **argv)
 {
-    unsigned char tableau[GRID_LENGHT][GRID_LENGHT] = { 0 };
+    unsigned char tableau[GRID_LENGTH][GRID_LENGTH] = { 0 };
     int locationX = 0;
     int locationY = 0;
 
@@ -20,27 +20,29 @@ int main(int argc, char **argv)
     placeBomb(tableau, BOMB_NUMBER);
 
     displayGrid(tableau);
-    printf("Choississez vos coordonnées (sous forme x : y) : ");
+    printf("Choississez vos coordonnees : \n");
     printf("x : ");
     scanf_s("%d", &locationX);
-    printf("y");
+    printf("y : ");
     scanf_s("%d", &locationY);
-    play(tableau, locationX, locationY);
-
+    if (locationX <= 10 || locationY <= 10)
+        play(tableau, locationX - 1, locationY - 1);
+    else
+        printf("Jouez des coordonnees comprises entre 1 et 10\n");
     displayGrid(tableau);
     return 0;
 }
 
-void displayGrid(unsigned char tableau[GRID_LENGHT][GRID_LENGHT])
+void displayGrid(unsigned char tableau[GRID_LENGTH][GRID_LENGTH])
 {
     printf("\n");
-    for (int z = 0; z < GRID_LENGHT; z++) {
+    for (int z = 0; z < GRID_LENGTH; z++) {
         printf("----");
     }
     printf("-\n");
-    for (int i = 0; i < GRID_LENGHT; i++) {
+    for (int i = 0; i < GRID_LENGTH; i++) {
         printf("|");
-        for (int y = 0; y < GRID_LENGHT; y++) {
+        for (int y = 0; y < GRID_LENGTH; y++) {
             switch (tableau[i][y]) {
                 case 0:
                     printf(" - |");
@@ -49,7 +51,7 @@ void displayGrid(unsigned char tableau[GRID_LENGHT][GRID_LENGHT])
                     printf(" - |");
                     continue;
                 case 9:
-                    printf("%d", tableau[i][y]);
+                    printf(" %d |", tableau[i][y]);
                     continue;
                 default:
                     printf(" %d |", tableau[i][y]);
@@ -57,21 +59,21 @@ void displayGrid(unsigned char tableau[GRID_LENGHT][GRID_LENGHT])
             }           
         }
         printf("\n");
-        for (int z = 0; z < GRID_LENGHT; z++) {
+        for (int z = 0; z < GRID_LENGTH; z++) {
             printf("----");
         }
         printf("-\n");
     }
 }
 
-void placeBomb(unsigned char tableau[GRID_LENGHT][GRID_LENGHT], unsigned int quantity)
+void placeBomb(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int quantity)
 {
     int bomb = 1;
 
     for (int i = 0; i < quantity; i++)
     {
-        int locationX = rand() % GRID_LENGHT;
-        int locationY = rand() % GRID_LENGHT;
+        int locationX = rand() % GRID_LENGTH;
+        int locationY = rand() % GRID_LENGTH;
         if (tableau[locationX][locationY] == 1) {
             i--;
             continue;
@@ -84,7 +86,23 @@ void placeBomb(unsigned char tableau[GRID_LENGHT][GRID_LENGHT], unsigned int qua
     }
 }
 
-void play(unsigned char tableau[GRID_LENGHT][GRID_LENGHT], unsigned int x, unsigned int y)
+void play(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int x, unsigned int y)
 {
     tableau[x][y] = 9;
+}
+
+int bombsAround(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int x, unsigned int y)
+{
+    unsigned int bombsAround = 0;
+    if (0 <= x - 1) {
+        if (tableau[x - 1][y] == 1) {
+            bombsAround++;
+        }
+    }
+    if (0 <= y - 1) {
+        if (tableau[x][y - 1] == 1) {
+            bombsAround++;
+        }
+    } 
+    if
 }
