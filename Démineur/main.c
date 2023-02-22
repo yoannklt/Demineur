1cab1da58a2fdfc878d26bb7883ffcef90b8bf19
yoannklt <ykerlogot@gaming.tech>
2023-02-22 09:59:45 +0100

    dazd

--- Démineur/main.c
+++ Démineur/main.c
@@ -3,16 +3,17 @@
 #include <time.h>
 
 #define GRID_LENGTH 10
-#define BOMB_NUMBER 17
+#define BOMB_NUMBER 5
 
-void displayGrid(unsigned char tableau[GRID_LENGTH][GRID_LENGTH]);
-void placeBomb(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int quantity);
-void play(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int x, unsigned int y);
-int bombsAround(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int x, unsigned int y);
+
+void displayGrid( char tableau[GRID_LENGTH][GRID_LENGTH]);
+void placeBomb( char tableau[GRID_LENGTH][GRID_LENGTH],  int quantity);
+void play( char tableau[GRID_LENGTH][GRID_LENGTH],  int x,  int y);
+int bombsAround( char tableau[GRID_LENGTH][GRID_LENGTH],  int x,  int y);
 
 int main(int argc, char **argv)
 {
-    unsigned char tableau[GRID_LENGTH][GRID_LENGTH] = { 0 };
+    char tableau[GRID_LENGTH][GRID_LENGTH] = { 0 };
     int locationX = 0;
     int locationY = 0;
 
@@ -21,6 +22,8 @@ int main(int argc, char **argv)
     placeBomb(tableau, BOMB_NUMBER);
 
     displayGrid(tableau);
+
+
     printf("Choississez vos coordonnees : \n");
     printf("x : ");
     scanf_s("%d", &locationX);
@@ -34,7 +37,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void displayGrid(unsigned char tableau[GRID_LENGTH][GRID_LENGTH])
+void displayGrid( char tableau[GRID_LENGTH][GRID_LENGTH])
 {
     printf("\n");
     for (int z = 0; z < GRID_LENGTH; z++) {
@@ -44,7 +47,6 @@ void displayGrid(unsigned char tableau[GRID_LENGTH][GRID_LENGTH])
     for (int i = 0; i < GRID_LENGTH; i++) {
         printf("|");
         for (int y = 0; y < GRID_LENGTH; y++) {
-            int bombs = bombsAround(tableau, i, y);
             switch (tableau[i][y]) {
                 case 0:
                     printf(" - |");
@@ -53,7 +55,8 @@ void displayGrid(unsigned char tableau[GRID_LENGTH][GRID_LENGTH])
                     printf(" - |");
                     continue;
                 case 9:
-                    printf(" %d |", bombs);
+                    if (tableau[i][y] != 1)
+                        printf(" %d |", bombsAround(tableau, i, y));
                     continue;
                 default:
                     printf(" %d |", tableau[i][y]);
@@ -68,7 +71,7 @@ void displayGrid(unsigned char tableau[GRID_LENGTH][GRID_LENGTH])
     }
 }
 
-void placeBomb(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int quantity)
+void placeBomb( char tableau[GRID_LENGTH][GRID_LENGTH],  int quantity)
 {
     int bomb = 1;
 
@@ -88,44 +91,56 @@ void placeBomb(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int qua
     }
 }
 
-void play(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int x, unsigned int y)
+void play(char tableau[GRID_LENGTH][GRID_LENGTH], int x, int y)
 {
+    int bombs = bombsAround(tableau, x, y);
+    
     tableau[x][y] = 9;
+
+    /*while (bombs == 0)
+    {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ()
+            }
+        }
+    }*/
 }
 
-int bombsAround(unsigned char tableau[GRID_LENGTH][GRID_LENGTH], unsigned int x, unsigned int y)
+int bombsAround(char tableau[GRID_LENGTH][GRID_LENGTH], int x, int y)
 {
-    unsigned int bombsAround = 0;
+    int bombsAround = 0;
 
-    if (0 <= x - 1) 
-        if (tableau[x - 1][y] == 1) 
+    if (x - 1 >= 0 ) {
+        if (tableau[x - 1][y] == 1)
             bombsAround++;
-        
-    if (0 <= y - 1) 
-        if (tableau[x][y - 1] == 1) 
-            bombsAround++;
-        
-    if (9 >= x + 1) 
+    }
+
+    if (GRID_LENGTH >= x + 1)
         if (tableau[x + 1][y] == 1)
             bombsAround++;
 
-    if (9 >= y + 1)
+    if (0 <= y - 1)
+        if (tableau[x][y - 1] == 1)
+            bombsAround++;
+
+    if (GRID_LENGTH >= y + 1)
         if (tableau[x][y + 1] == 1)
+            bombsAround++;
+
+    if (GRID_LENGTH >= x + 1 && GRID_LENGTH >= y + 1)
+        if (tableau[x + 1][y + 1] == 1)
             bombsAround++;
 
     if (0 <= x - 1 && 0 <= y - 1)
         if (tableau[x - 1][y - 1] == 1)
             bombsAround++;
-    
-    if (0 >= x + 1 && 0 >= y + 1)
-        if (tableau[x + 1][y + 1] == 1)
-            bombsAround++;
 
-    if (0 <= x - 1 && 9 >= y + 1)
+    if (0 <= x - 1 && GRID_LENGTH >= y + 1)
         if (tableau[x - 1][y + 1] == 1)
             bombsAround++;
 
-    if (0 <= y - 1 && 9 >= x + 1)
+    if (0 <= y - 1 && GRID_LENGTH >= x + 1)
         if (tableau[x + 1][y - 1] == 1)
             bombsAround++;
 
