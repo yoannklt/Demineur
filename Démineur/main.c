1ebda6bd9c91895511f8ced9783f743445e291fd
yoannklt <ykerlogot@gaming.tech>
2023-02-23 10:26:25 +0100

    dzad

--- Démineur/main.c
+++ Démineur/main.c
@@ -9,7 +9,7 @@
 #define HIDDEN_CELL 0
 
 void displayGrid( char tableau[GRID_LENGTH][GRID_LENGTH]);
-void placeBomb( char tableau[GRID_LENGTH][GRID_LENGTH],  int quantity);
+void placeBomb( char tableau[GRID_LENGTH][GRID_LENGTH],  int quantity, int x, int y);
 int play( char tableau[GRID_LENGTH][GRID_LENGTH], int x,  int y);
 int bombsAround( char tableau[GRID_LENGTH][GRID_LENGTH],  int x,  int y);
 
@@ -18,24 +18,38 @@ int main(int argc, char **argv)
     char tableau[GRID_LENGTH][GRID_LENGTH] = { 0 };
     int locationX = 0;
     int locationY = 0;
+    int lap = 0;
 
     srand(time(NULL));
 
-    placeBomb(tableau, BOMB_NUMBER);
-
     displayGrid(tableau);
-
-
     printf("Choississez vos coordonnees : \n");
     printf("x : ");
     scanf_s("%d", &locationX);
     printf("y : ");
     scanf_s("%d", &locationY);
-    if (locationX <= 10 || locationY <= 10)
-        play(tableau, locationX - 1, locationY - 1);
-    else
-        printf("Jouez des coordonnees comprises entre 1 et 10\n");
+
+    placeBomb(tableau, BOMB_NUMBER, locationX - 1, locationX - 1);
+    play(tableau, locationX - 1, locationY - 1);
+
     displayGrid(tableau);
+    printf("TABLEAU AVANT PLAY\n");
+
+    while (1) {
+        printf("Choississez vos coordonnees : \n");
+        printf("x : ");
+        scanf_s("%d", &locationX);
+        printf("y : ");
+        scanf_s("%d", &locationY);
+
+
+        play(tableau, locationX - 1, locationY - 1);
+
+        displayGrid(tableau);
+        printf("TABLEAU APRES PLAY\n");
+        lap++;
+    }
+   
     return 0;
 }
 
@@ -46,7 +60,7 @@ void displayGrid( char tableau[GRID_LENGTH][GRID_LENGTH])
         printf("----");
     }
     printf("-\n");
-    for (int i = 0; i < GRID_LENGTH; i++) {
+    for (int i  = 0; i < GRID_LENGTH; i++) {
         printf("|");
         for (int y = 0; y < GRID_LENGTH; y++) {
             switch (tableau[i][y]) {
@@ -54,12 +68,13 @@ void displayGrid( char tableau[GRID_LENGTH][GRID_LENGTH])
                     printf(" - |");
                     continue;
                 case BOMB_CELL:
-                    printf(" - |");
+                    printf(" X |");
                     continue;
                 case DISCOVERED_CELL:
-                    printf(" %d |", bombsAround(tableau, i, y));
+                    printf(" O |");
+                    continue;
                 default:
-                    printf(" %d |", tableau[i][y]);
+                    printf(" %d |", tableau[i][y] - 3);
                     continue;
             }           
         }
@@ -71,7 +86,7 @@ void displayGrid( char tableau[GRID_LENGTH][GRID_LENGTH])
     }
 }
 
-void placeBomb( char tableau[GRID_LENGTH][GRID_LENGTH],  int quantity)
+void placeBomb( char tableau[GRID_LENGTH][GRID_LENGTH], int quantity, int x, int y)
 {
     int bomb = 1;
 
@@ -79,39 +94,40 @@ void placeBomb( char tableau[GRID_LENGTH][GRID_LENGTH],  int quantity)
     {
         int locationX = rand() % GRID_LENGTH;
         int locationY = rand() % GRID_LENGTH;
-        if (tableau[locationX][locationY] == BOMB_CELL) {
+        if (tableau[locationY][locationX] == BOMB_CELL && (x - 1 <= locationX && locationX <= x + 1 && y - 1 <= locationY && locationY <= y + 1)) {
             i--;
             continue;
         }
         else {
             printf("Bombe numero %d : %d | %d\n", bomb, locationX, locationY);
-            tableau[locationX][locationY] = BOMB_CELL;
+            tableau[locationY][locationX] = BOMB_CELL;
             bomb++;
         }
     }
 }
 
+
 int play(char tableau[GRID_LENGTH][GRID_LENGTH],  int x,  int y)
 {
-    tableau[x][y] = DISCOVERED_CELL;
-
     int bombs = bombsAround(tableau, x, y);
+    printf("BEBONS %d\n", bombs);
 
     if (bombs != 0) {
-        tableau[x][y] = DISCOVERED_CELL;
+        tableau[y][x] = bombs + 3;
         return 1;
     }
 
+    tableau[y][x] = DISCOVERED_CELL;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (i - 1 != 0 || j - 1 != 0) {
-                if ((x + i - 1 >= 0 && x + i - 1 < GRID_LENGTH && y + j - 1 >= 0 && y + j - 1 < GRID_LENGTH) && tableau[y + j - 1][x + i - 1] == BOMB_CELL) {
-                    tableau[i][j] = DISCOVERED_CELL;
-                    play(tableau, i, j);
+                if ((x + i - 1 >= 0 && x + i - 1 < GRID_LENGTH && y + j - 1 >= 0 && y + j - 1 < GRID_LENGTH) && tableau[y + j - 1][x + i - 1] == HIDDEN_CELL) {
+                    play(tableau, x + i - 1, y + j - 1);
                 }
             }
         }
     }
+    return 0;
 }
 
 int bombsAround( char tableau[GRID_LENGTH][GRID_LENGTH],  int x,  int y)
